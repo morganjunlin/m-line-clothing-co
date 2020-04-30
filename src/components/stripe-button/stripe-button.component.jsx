684d33b18bca1324cb9d0548c17a28237856baf9
Morgan Lin <morganjunlin@gmail.com>
2020-04-30 02:52:46 -0700

    [feat] integrates stripe and updates favicon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_qLPazGbimyhwLnaBKQed8wKk00XYZWvUGc';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesfful')
+  }
+
+  return (
+    <StripeCheckout 
+      label='Pay Now'
+      name='Monesprit Ltd.'
+      billingAddress
+      shippingAddress
+      description={`Your total is ${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
